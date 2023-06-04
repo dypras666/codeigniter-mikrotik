f25abf490f4d2ffe4c827ed09ed7e906cd0f215a
Kurniawan <dypras666@gmail.com>
2023-06-04 09:07:48 +0700

    remove db auto load

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','RouterApi','session','fcm');
+$autoload['libraries'] = array('RouterApi','session','fcm');
 
 /*
 | -------------------------------------------------------------------
